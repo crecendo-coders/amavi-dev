4a3cb0e6f2366bb4427d01669567ebf87ea02337
Caleb Ratelle <caleb.ratelle@gmail.com>
2023-10-09 16:36:41 -0600

    Backend folders added, model.js made with dbvisualizer info, and seed file made with dummy data

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import ViteExpress from 'vite-express';
+import session from "express-session";
+
+const app = express();
+const PORT = 2319;
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(session({
+    saveUninitialized: true,
+    resave: false,
+    secret: 'aslkdfjahsdk',
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 48
+    }
+}));
+
+// Routes Go Here
+
+
+// Authentication endpoints Go Here
+
+
+ViteExpress.listen(app, PORT, () => console.log(`Server is running on port ${PORT}!`));
