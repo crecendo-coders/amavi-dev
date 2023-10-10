d45dfaf605d1a9d09d4aeea5887017866bad3583
Caleb Ratelle <caleb.ratelle@gmail.com>
2023-10-10 14:08:18 -0600

    big fix on index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 import express from "express";
 import ViteExpress from 'vite-express';
 import session from "express-session";
+import { fileURLToPath } from 'url'; // Import the fileURLToPath function
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url); // Get the current file's path
+const __dirname = path.dirname(__filename); // Get the current directory's path
 
 const app = express();
 const PORT = 2319;
@@ -17,10 +22,11 @@ app.use(session({
     }
 }));
 
+// Serve static files from a directory (e.g., public)
+app.use(express.static(path.join(__dirname, '../index.html')));
+
 // Routes Go Here
 
-
 // Authentication endpoints Go Here
-
 
 ViteExpress.listen(app, PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
