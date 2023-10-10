3721fef9efea3f6343e27cd22a0c6027db6af0b9
dan-goodrick <dannyjeee@yahoo.com>
2023-10-10 14:14:20 -0600

    stuff for aws

--- server/index.js
+++ server/index.js
@@ -1,26 +1,23 @@
 import express from "express";
 import ViteExpress from 'vite-express';
 import session from "express-session";
+import { fileURLToPath } from 'url'; // Import the fileURLToPath function
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url); // Get the current file's path
+const __dirname = path.dirname(__filename); // Get the current directory's path
 
 const app = express();
 const PORT = 2319;
-
-// Middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(session({
-    saveUninitialized: true,
-    resave: false,
-    secret: 'aslkdfjahsdk',
-    cookie: {
-        maxAge: 1000 * 60 * 60 * 48
+	app.use(session({
     }
-}));
+));
+
+// Serve static files from a directory (e.g., public)
+app.use(express.static(path.join(__dirname, '../index.html')));
 
 // Routes Go Here
 
-
 // Authentication endpoints Go Here
 
-
-ViteExpress.listen(app, PORT, () => console.log(`Server is running on port ${PORT}!`));
+ViteExpress.listen(app, PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
