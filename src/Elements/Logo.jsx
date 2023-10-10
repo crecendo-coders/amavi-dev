08c0a4515c9c1cf0471c844d7f96e96a17af0a9d
dan-goodrick <dannyjeee@yahoo.com>
2023-10-10 13:43:41 -0600

    adds logo template

---
+++ src/Elements/Logo.jsx
@@ -0,0 +1,17 @@
+const Logo = () => { 
+  return (
+    <div className="flex items-center flex-shrink-0 text-white mr-6">
+    <a href="/" className="custom-logo-link" rel="home">
+      <img
+        width="512"
+        height="512"
+        src="logo.gif"
+        className="custom-logo"
+        alt="Amavi"
+      />
+    </a>
+  </div>
+  )
+ }
+
+ export default Logo
--- src/Elements/NavBar.jsx
+++ src/Elements/NavBar.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { useState } from "react";
 import axios from "axios";
+import Logo from "./Logo";
 
 const navLinks = [
   { name: "Events", route: "/events" },
@@ -33,29 +34,8 @@ const NavBar = () => {
   return (
       <nav className="bg-teal-500 p-6">
         <div className="flex items-center justify-between">
-          <div className="flex items-center flex-shrink-0 text-white mr-6">
-            <a href="/" className="custom-logo-link" rel="home">
-              <img
-                width="512"
-                height="512"
-                src="logo.gif"
-                className="custom-logo"
-                alt="Amavi"
-              />
-            </a>
-          </div>
-          <div className="block lg:hidden">
-            <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-              <svg
-                className="fill-current h-3 w-3"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <title>Menu</title>
-                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-              </svg>
-            </button>
-          </div>
+          <Logo />
+
           <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
             <div className="text-sm lg:flex-grow">
               <a
