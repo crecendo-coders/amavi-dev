626190982ad2a63b621f710552a9366d9bbef858
dan-goodrick <dannyjeee@yahoo.com>
2023-10-10 16:27:43 -0600

    adds footer

--- src/Elements/Footer.jsx
+++ src/Elements/Footer.jsx
@@ -1,83 +1,81 @@
 const Footer = () => {
-  <footer id="primary">
-    <section id="footer">
-      <div class="wrapper">
-        <div class="flex">
-          <a href="/mailing-list" id="footerMailingList">
-            <h3>Join our Mailing List</h3>
-            <p>
-              Subscribe to receive advance notice of concerts, and occasional
-              news from the group.
-            </p>
-          </a>
+  return (
+    <footer id="primary">
+      <section id="footer">
+        <div class="wrapper">
+          <div class="flex">
+            <a href="/mailing-list" id="footerMailingList">
+              <h3>Join our Mailing List</h3>
+              <p>
+                Subscribe to receive advance notice of concerts, and occasional
+                news from the group.
+              </p>
+            </a>
 
-          <a href="/support" id="footerDonate">
-            <h3>Donate to Amavi</h3>
-            <p>
-              Please help sustain the choir’s essential music education work and
-              artistic output.
-            </p>
-          </a>
+            <a href="/support" id="footerDonate">
+              <h3>Donate to Amavi</h3>
+              <p>
+                Please help sustain the choir’s essential music education work
+                and artistic output.
+              </p>
+            </a>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
 
-    <section id="legal">
-      <div class="wrapper">
-        <nav id="secondary" class="menu-secondary-container">
-          <ul id="menu-secondary" class="">
-            <li
-              id="menu-item-12413"
-              class="menu-item menu-item-type-post_type menu-item-object-page menu-item-12413"
-            >
-              <a href="https://www.tenebrae-choir.com/contact">Contact</a>
-            </li>
+      <section id="legal">
+        <div class="wrapper">
+          <nav id="secondary" class="menu-secondary-container">
+            <ul id="menu-secondary" class="">
+              <li
+                id="menu-item-12413"
+                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-12413"
+              >
+                <a href="/contact">Contact</a>
+              </li>
 
-            <li
-              id="menu-item-60"
-              class="menu-item menu-item-type-post_type menu-item-object-page menu-item-60"
-            >
-              <a href="https://www.tenebrae-choir.com/privacy-policy">
-                Privacy Policy
-              </a>
-            </li>
-            <li
-              id="menu-item-11279"
-              class="menu-item menu-item-type-post_type menu-item-object-page menu-item-11279"
-            >
-              <a href="https://www.tenebrae-choir.com/equality-diversity-policy">
-                Equality &amp; Diversity Policy
-              </a>
-            </li>
-            <li
-              id="menu-item-61"
-              class="menu-item menu-item-type-post_type menu-item-object-page menu-item-61"
-            >
-              <a href="https://www.tenebrae-choir.com/terms-conditions">
-                Terms &amp; Conditions
-              </a>
-            </li>
-            <li
-              id="menu-item-59"
-              class="menu-item menu-item-type-post_type menu-item-object-page menu-item-59"
-            >
-              <a href="https://www.tenebrae-choir.com/credits">Credits</a>
-            </li>
-          </ul>
-        </nav>
-        <p id="copyright">Copyright 2023 Tenebrae. All Rights Reserved.</p>
+              <li
+                id="menu-item-60"
+                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-60"
+              >
+                <a href="/privacy-policy">
+                  Privacy Policy
+                </a>
+              </li>
+              <li
+                id="menu-item-11279"
+                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-11279"
+              >
+                <a href="/equality-diversity-policy">
+                  Equality &amp; Diversity Policy
+                </a>
+              </li>
+              <li
+                id="menu-item-61"
+                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-61"
+              >
+                <a href="/terms-conditions">
+                  Terms &amp; Conditions
+                </a>
+              </li>
+              <li
+                id="menu-item-59"
+                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-59"
+              >
+                <a href="/credits">Credits</a>
+              </li>
+            </ul>
+          </nav>
+          <p id="copyright">Copyright 2023 Amavi. All Rights Reserved.</p>
 
-        <p>
-          Tenebrae is a working name of The Tenebrae Choir, Registered in
-          England and Wales, Company No. 7549890 (Limited by Guarantee),
-          Registered Charity No. 1142138. Tenebrae Choir Trading Limited,
-          Registered in England and Wales, Company No. 7549484. Registered
-          Office: c/o Lakin Rose Limited, Pioneer House Vision Park Histon,
-          Cambridge, Cambridgeshire, CB24 9NL.
-        </p>
-      </div>
-    </section>
-  </footer>;
+          <p>
+            Amavi is a working name of The Amavi Chorale, 501(c)(3) Registration: ##-#######. 
+            Office: ***************Address**********.
+          </p>
+        </div>
+      </section>
+    </footer>
+  );
 };
 
 export default Footer;
