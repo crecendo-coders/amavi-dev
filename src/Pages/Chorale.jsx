ba2ef8c01af5a64923177c78cd379984ed640d55
dan-goodrick <dannyjeee@yahoo.com>
2023-10-11 15:48:45 -0600

    Adds content to the about the chorale page

--- src/Pages/Chorale.jsx
+++ src/Pages/Chorale.jsx
@@ -1,5 +1,22 @@
 export default function AboutChorale() {
   return (
-    <div>AboutChorale</div>
+    <>
+
+    <h2>About the Amavi Chorale</h2>
+    <h3>Our Motivation</h3>
+    <div>Founded in 2017, Amavi Chorale is dedicated to creating meaningful musical experiences by giving first rate performances of the finest choral literature. As artists, educators, and advocates of the choral art, the Amavi Chorale aspires to reach audiences of all ages, backgrounds and socio-economic circumstances. </div>
+    <img src="about-chorale-organ.jpeg" alt="Chorale Organ"/>
+    <div>The name Amavi (lat. I loved) illustrates how we are motivated by our love for music, people, all things beautiful, and our love for God.</div>
+    <h2>Our Mission</h2>
+    <img src="about-chorale-slc.jpeg" alt="SLC"/>
+    <div>Amavi Chorale aspires to contribute to an already rich choral tradition along the Utah Wasatch Front by:</div>
+    <ul>
+      <li>Singing the finest choral repertoire </li>
+      <li>Collaborating with local and national artists </li>
+      <li>Providing a space for creativity and artistic outlet for our singers, staff, and audiences</li>
+    </ul>
+    <img src="chorale-wide.jpeg" alt="chorale"/>
+
+    </>
   )
 }
