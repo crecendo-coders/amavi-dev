290a6e3ab1672f16c892e816b2d5593c7bbfb8e9
Caleb Ratelle <caleb.ratelle@gmail.com>
2023-10-11 08:56:06 -0600

    port changed to 4001

--- server/index.js
+++ server/index.js
@@ -1,14 +1,10 @@
 import express from "express";
 import ViteExpress from 'vite-express';
 import session from "express-session";
-import { fileURLToPath } from 'url'; // Import the fileURLToPath function
-import path from "path";
 
-const __filename = fileURLToPath(import.meta.url); // Get the current file's path
-const __dirname = path.dirname(__filename); // Get the current directory's path
 
 const app = express();
-const PORT = 2319;
+const PORT = 4001;
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
@@ -22,8 +18,6 @@ app.use(session({
     }
 }));
 
-// Serve static files from a directory (e.g., public)
-app.use(express.static(path.join(__dirname, '../index.html')));
 
 // Routes Go Here
 
