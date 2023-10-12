ea43392d784f32f2cee3c20ae6af6c7d8155b8b4
dan-goodrick <dannyjeee@yahoo.com>
2023-10-12 15:26:33 -0600

    adds emailer to audition post

--- server/controllers/audition.js
+++ server/controllers/audition.js
@@ -1,4 +1,18 @@
+import nodemailer from 'nodemailer'
+import previewEmail from 'preview-email'
+import dotenv from 'dotenv'
+dotenv.config()
 import { Audition, Event } from "../model.js";
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465, //587 is default for insecure
+  secure: true,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
 
 export default {
   post: (req, res) => {
@@ -10,6 +24,18 @@ export default {
       })
       .catch((error) => {
         console.error(`Unable to Add Person ${req.body}`, error);
-      });
+      }).then(async () => { 
+        const message = {
+          from: {mail: "Amavi Chorale",
+          address: process.env.EMAIL}, // sender address
+            to: "dannyjeee@yahoo.com", // list of receivers
+            subject: "We have recieved your audition request ✔", // Subject line
+            text: "Greetings.  Let me know when you would like to meet", // plain text body
+            html: "<b>HTML</b>", // html body
+          }
+        previewEmail(message).then(console.log).catch(console.error)
+        await transporter.sendMail(message);
+       }
+      )
   },
 };
