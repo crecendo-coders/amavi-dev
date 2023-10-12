ecc11c78b33f510d9977e97036c0caa20e22c563
dan-goodrick <dannyjeee@yahoo.com>
2023-10-12 10:43:41 -0600

    adds react hook form

--- src/Elements/FormElements/Error.jsx
+++
@@ -1,9 +0,0 @@
-function Error(props) {
-  return (
-    <div className='text-rose-600 '>
-        {props.children}
-    </div>
-  )
-}
-
-export default Error
--- src/Pages/Audition.jsx
+++ src/Pages/Audition.jsx
@@ -1,120 +1,58 @@
-import { Form, Formik } from "formik";
-import { object, string, number, array } from "yup";
+import { useState } from "react";
+import { useForm, useController } from "react-hook-form";
+import "./../../css/form.css";
+function Input(props) {
+  const { field, fieldState } = useController(props);
 
-import Swal from "sweetalert2";
-import withReactContent from "sweetalert2-react-content";
-import FormInput from "../Elements/FormElements/FormInput";
+  return (
+    <div>
+      <input {...field} placeholder={props.name} />
+      <p>{fieldState.isTouched && "Touched"}</p>
+      <p>{fieldState.isDirty && "Dirty"}</p>
+      <p>{fieldState.invalid ? "invalid" : "valid"}</p>
+    </div>
+  );
+}
 
 export default function Audition() {
-  const Confirm = withReactContent(Swal);
-  const onSubmit = (values) => {
-    console.log("Submitted Values:", values);
-    Confirm.fire({
-      icon: "success",
-      title: "Thank you for applying",
-      showConfirmButton: true,
-    });
-  };
-  const dropdownOptions = [
-    { key: "How did you hear about us?", value: "" },
-    { key: "Friend", value: "friend" },
-    { key: "Event", value: "event" },
-    { key: "Social Media", value: "social" },
-    { key: "Advertisement", value: "ad" },
-    { key: "Other", value: "other" },
-  ];
-  const checkboxOptions = [
-    { key: "Select your range(s)", value: "" },
-    { key: "1st Soprano", value: "soprano1" },
-    { key: "2nd Soprano", value: "soprano2" },
-    { key: "1st Alto", value: "alto1" },
-    { key: "2nd Alto", value: "alto2" },
-    { key: "1st Tenor", value: "tenor1" },
-    { key: "2nd Tenor", value: "tenor2" },
-    { key: "Baritone", value: "baritone" },
-    { key: "Bass", value: "bass" },
-  ];
-  const initialValues = {
-    name: "",
-    email: "",
-    phone: "",
-    age: "",
-    experience: "",
-    expDetail: "",
-    training: "",
-    connection: "",
-    checkboxOption: [],
-  };
-  const validationSchema = object({
-    name: string().required("Required"),
-    email: string().email("Invalid email").required("Required").nullable(),
-    phone: string().required("Required").nullable(),
-    age: number(),
-    experience: number(),
-    expDetail: string(),
-    training: string(),
-    connection: string(),
-    checkboxOption: array().required("Required"),
-  });
+  const { register, handleSubmit } = useForm();
+  const [data, setData] = useState("");
+
   return (
-    <>
-      <h1>Audition</h1>
-      <div>
-        Amavi Chorale aspires to contribute to an already rich choral tradition
-        along the Utah Wasatch Front. An audition is required to join the choir.
-        To request an audition, fill out the form below.
-      </div>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={onSubmit}
-      >
-        {(formik) => {
-          return (
-            <Form>
-              <FormInput
-                control="input"
-                label="First and Last Name"
-                name="name"
-              />
-              <FormInput
-                control="input"
-                type="email"
-                label="Email"
-                name="email"
-              />
-              <FormInput
-                control="input"
-                type="number"
-                label="Years of Experience"
-                name="experience"
-              />
-              <FormInput control="input" label="Phone" name="phone" />
-              <FormInput
-                control="textarea"
-                label="Description of Experience"
-                name="expDetail"
-              />
-              <FormInput control="textarea" label="Training" name="training" />
-              <FormInput
-                control="select"
-                label="How did you hear about us?"
-                name="connection"
-                options={dropdownOptions}
-              />
-              <FormInput
-                control="checkbox"
-                label="Vocal range(s)"
-                name="checkboxOption"
-                options={checkboxOptions}
-              />
-              <button type="submit" disabled={!formik.isValid}>
-                Submit
-              </button>
-            </Form>
-          );
-        }}
-      </Formik>
-    </>
+    <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+    Salutation
+      <select {...register("title", { required: true })}>
+        <option value="Mr">Mr</option>
+        <option value="Mrs">Mrs</option>
+        <option value="Miss">Miss</option>
+        <option value="Dr">Dr</option>
+      </select>
+      <Input control={control} type="text" placeholder="First and Last Name" {...register("First and Last Name", {required: true, maxLength: 100})} />
+      <Input control={control} type="text" placeholder="Email" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} />
+      <Input control={control} type="tel" placeholder="Phone" {...register("Phone", {required: true})} />
+      <Input control={control} type="number" placeholder="Years of Experience" {...register} />
+      <Input control={control} type="text" placeholder="Description of Vocal Experience" {...register} />
+      
+      <select placeholder="How did you hear about us?"{...register("connection")}>
+        <option value="Friend">Friend</option>
+        <option value=" Event"> Event</option>
+        <option value=" Social Media"> Social Media</option>
+        <option value=" Advertisement"> Advertisement</option>
+        <option value=" Other"> Other</option>
+      </select>
+      Vocal Range(s)
+      Soprano I
+      <Input control={control} type="checkbox" placeholder="Soprano I" {...register("Soprano I", {})} />
+      <Input control={control} type="checkbox" placeholder="Soprano II" {...register("Soprano II", {})} />
+      <Input control={control} type="checkbox" placeholder="Alto I" {...register("Alto I", {})} />
+      <Input control={control} type="checkbox" placeholder="Alto II" {...register("Alto II", {})} />
+      <Input control={control} type="checkbox" placeholder="Tenor I" {...register("Tenor I", {})} />
+      <Input control={control} type="checkbox" placeholder="Tenor II" {...register("Tenor II", {})} />
+      <Input control={control} type="checkbox" placeholder="Baritone" {...register("Baritone", {})} />
+      <Input control={control} type="checkbox" placeholder="Bass" {...register("Bass", {})} />
+
+      <p>{data}</p>
+      <input type="submit" />
+    </form>
   );
 }
