769583eb07f5ac1c9e004ed775fd8e38ca811946
Adam Johnson <adaojohnson902@gmail.com>
2023-10-17 15:40:20 -0600

    implemented headless ui to be able to style the about button

--- src/Elements/Logout.jsx
+++ src/Elements/Logout.jsx
@@ -2,14 +2,24 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { NavLink } from "react-router-dom";
 
-const LogoutButton = () => {
-  const { logout } = useAuth0();
+const activeNav =
+    "bg-blue-500 text-lg m-2 text-white px-4 py-4 rounded-full underline  focus:text-white hover:text-white";
+const inactiveNav =
+    "bg-blue-500 text-lg m-2 text-white px-4 py-4 rounded-full hover:bg-white no-underline hover:no-underline focus:no-underline";
 
-  return (
-    <NavLink onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
-      Log Out
-    </NavLink>
-  );
+const LogoutButton = () => {
+    const { logout } = useAuth0();
+
+    return (
+        <NavLink
+            className={({ isActive }) => (isActive ? inactiveNav : inactiveNav)}
+            onClick={() =>
+                logout({ logoutParams: { returnTo: window.location.origin } })
+            }
+        >
+            Log Out
+        </NavLink>
+    );
 };
 
 export default LogoutButton;
