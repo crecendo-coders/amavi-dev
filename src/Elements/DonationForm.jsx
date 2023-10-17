2209818ab610723905873ba5d4a1c897a95985b6
Adam Johnson <adaojohnson902@gmail.com>
2023-10-17 12:32:39 -0600

    slight styling of donation button on page

--- src/Elements/DonationForm.jsx
+++ src/Elements/DonationForm.jsx
@@ -1,23 +1,29 @@
 ﻿import React, { useState, useEffect } from "react";
 
 const DonationForm = () => (
-  <section>
-    <div className="product">
+  <div className="flex justify-center">
+    <div className="flex flex-col align-middle">
+
+    <div className="product flex flex-col align-middle w-[500px]">
       <img
-        src="https://i.imgur.com/EHyR2nP.png"
-        alt="The cover of Stubborn Attachments"
-      />
+        src="amavi-big-logo.png"
+        alt="Insert a donation image"
+        className="h-36"
+        />
       <div className="description">
       <h3>Please consider donating to support</h3>
-      <h5>$20.00</h5>
       </div>
     </div>
-    <form action="/create-checkout-session" method="POST">
-      <button type="submit">
-        Checkout
+    <form action="/create-checkout-session" method="POST" className="flex flex-col">
+      <div className="flex justify-center">
+      <button type="submit" className="p-8">
+        Donate
       </button>
+      </div>
+      <div>This button will take you to the stripe website for secure payments.</div>
     </form>
-  </section>
+        </div>
+  </div>
 );
 
 export default DonationForm
