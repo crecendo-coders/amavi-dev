dc3ce56330ff7737abe1a0ecf9c3b123beea4384
Adam Johnson <adaojohnson902@gmail.com>
2023-10-17 16:04:20 -0600

    navbar styling

--- src/Elements/NavBar.jsx
+++ src/Elements/NavBar.jsx
@@ -8,9 +8,9 @@ import Admin from "../Pages/Admin";
 import { Menu } from "@headlessui/react";
 
 const activeNav =
-    "bg-blue-500 text-lg text-white m-2 px-4 py-4 rounded-full underline  focus:text-white hover:text-white";
+    "bg-blue-500 text-lg text-white m-2 mx-6 px-4 py-4 rounded-full underline  focus:text-white hover:text-white";
 const inactiveNav =
-    "bg-blue-500 text-lg text-white m-2 px-4 py-4 rounded-full hover:bg-white no-underline hover:no-underline focus:no-underline";
+    "bg-blue-500 text-lg text-white m-2 mx-6 px-4 py-4 rounded-full hover:bg-white no-underline hover:no-underline focus:no-underline";
 
 const transitionActive =
     "text-white hover:text-gray-300 transition duration-300";
@@ -19,160 +19,121 @@ const NavBar = () => {
     const { user } = useAuth0();
     return (
         <nav className="bg-blue-500 py-2 sticky top-0 z-10">
-            <div className="container mx-auto flex flex-row">
-                <div className="flex flex-row">
-                    <div className=" flex flex-row">
-                        <NavLink
-                            to="/events"
-                            className={({ isActive }) =>
-                                isActive
-                                    ? "bg-blue-500 text-lg m-2 text-white px-4 py-4 rounded-full underline  focus:text-white hover:text-white"
-                                    : "bg-blue-500 text-lg m-2 text-white px-4 py-4 rounded-full hover:bg-white no-underline hover:no-underline focus:no-underline"
-                            }
-                        >
-                            Events
-                        </NavLink>
-                        <NavLink
-                            to="/support"
-                            className={({ isActive }) =>
-                                isActive ? activeNav : inactiveNav
-                            }
-                        >
-                            Support Us
-                        </NavLink>
-                        
+            <div className=" flex flex-row justify-between">
+                <div className=" flex items-center flex-row mx-12">
+                    <NavLink
+                        to="/events"
+                        className={({ isActive }) =>
+                            isActive
+                                ? "bg-blue-500 text-lg m-2 text-white px-4 py-4 rounded-full underline  focus:text-white hover:text-white"
+                                : "bg-blue-500 text-lg m-2 text-white px-4 py-4 rounded-full hover:bg-white no-underline hover:no-underline focus:no-underline"
+                        }
+                    >
+                        Events
+                    </NavLink>
+                    <NavLink
+                        to="/support"
+                        className={({ isActive }) =>
+                            isActive ? activeNav : inactiveNav
+                        }
+                    >
+                        Support Us
+                    </NavLink>
 
-                        <Menu as='div'
-                        className='relative rounded-full'
+                    <Menu as="div" className="relative rounded-full mx-6">
+                        <Menu.Button
+                            className={`appearance-none text-black flex align-middle p-4 hover:underline whitespace-nowrap m-4 rounded-full`}
                         >
-                            <Menu.Button className={`appearance-none text-black flex align-middle p-4 hover:underline whitespace-nowrap m-4 rounded-full`}>About</Menu.Button>
+                            About
+                        </Menu.Button>
 
-                            <Menu.Items className={`absolute right--4 w-32 origin-top-right rounded-xl divide-y bg-white divide-black/50 ring-1 ring-black ring-opacity-50 focus:outline-none`}>
-                                <Menu.Item>
-                                    {({ active }) => (
-                                        <NavLink
-                                            to="/affiliates"
-                                            className={`${
-                                                active
-                                                    ? `text-black `
-                                                    : `text-black `
-                                            } group flex w-full items-center rounded-lg px-2 py-2 text-sm`}
-                                        >
-                                            Our Supporters
-                                        </NavLink>
-                                    )}
-                                </Menu.Item>
-                                <Menu.Item>
-                                {({ active }) => (
-                                  <NavLink
-                                      to="/about-conductor"
-                                      className={`${
-                                          active
-                                              ? `${
-                                                    location.pathname ===
-                                                    "/home"
-                                                        ? "bg-calPoly/80 text-white"
-                                                        : "bg-third text-white"
-                                                }`
-                                              : `${
-                                                    location.pathname ===
-                                                    "/home"
-                                                        ? " text-white"
-                                                        : " text-black"
-                                                }`
-                                      } group flex w-full items-center rounded-lg px-2 py-2 text-sm`}
-                                  >
-                                      Conductor
-                                  </NavLink>
-                              )}
-                                    
-                                </Menu.Item>
-                                <Menu.Item>
-                                {({ active }) => (
-                                  <NavLink
-                                      to="/about-artists"
-                                      className={`${
-                                          active
-                                              ? `${
-                                                    location.pathname ===
-                                                    "/home"
-                                                        ? "bg-calPoly/80 text-white"
-                                                        : "bg-third text-white"
-                                                }`
-                                              : `${
-                                                    location.pathname ===
-                                                    "/home"
-                                                        ? " text-white"
-                                                        : " text-black"
-                                                }`
-                                      } group flex w-full items-center rounded-lg px-2 py-2 text-sm`}
-                                  >
-                                      Guest Artists
-                                  </NavLink>
-                              )}
-                                    
-                                </Menu.Item>
-                                <Menu.Item>
-                                {({ active }) => (
-                                  <NavLink
-                                      to="/about-chorale"
-                                      className={`${
-                                          active
-                                              ? `${
-                                                    location.pathname ===
-                                                    "/home"
-                                                        ? "bg-calPoly/80 text-white"
-                                                        : "bg-third text-white"
-                                                }`
-                                              : `${
-                                                    location.pathname ===
-                                                    "/home"
-                                                        ? " text-white"
-                                                        : " text-black"
-                                                }`
-                                      } group flex w-full items-center rounded-lg px-2 py-2 text-sm`}
-                                  >
-                                      Chorale
-                                  </NavLink>
-                              )}
-                                </Menu.Item>
-                                <Menu.Item>
-                                {({ active }) => (
-                                  <NavLink
-                                      to="/about-executive-team"
-                                      className={`${
-                                          active
-                                              ? `${
-                                                    location.pathname ===
-                                                    "/home"
-                                                        ? "bg-calPoly/80 text-white"
-                                                        : "bg-third text-white"
-                                                }`
-                                              : `${
-                                                    location.pathname ===
-                                                    "/home"
-                                                        ? " text-white"
-                                                        : " text-black"
-                                                }`
-                                      } group flex w-full items-center rounded-lg px-2 py-2 text-sm`}
-                                  >
-                                      Executive Team
-                                  </NavLink>
-                              )}
-             
-                                </Menu.Item>
-                                
-                            </Menu.Items>
-                        </Menu>
-
-                        <NavLink
-                            to="/audition"
-                            className={({ isActive }) =>
-                                isActive ? activeNav : inactiveNav
-                            }
+                        <Menu.Items
+                            className={`absolute right--4 w-32 origin-top-right rounded-xl divide-y bg-white divide-black/50 ring-1 ring-black ring-opacity-50 focus:outline-none`}
                         >
-                            Join Us
-                        </NavLink>
+                            <Menu.Item>
+                                {({ active }) => (
+                                    <NavLink
+                                        to="/affiliates"
+                                        className={`${
+                                            active
+                                                ? `text-black `
+                                                : `text-black `
+                                        } group flex w-full items-center rounded-lg px-2 py-2 text-sm`}
+                                    >
+                                        Our Supporters
+                                    </NavLink>
+                                )}
+                            </Menu.Item>
+                            <Menu.Item>
+                                {({ active }) => (
+                                    <NavLink
+                                        to="/about-conductor"
+                                        className={`${
+                                            active
+                                                ? `text-black `
+                                                : `text-black `
+                                        } group flex w-full items-center rounded-lg px-2 py-2 text-sm`}
+                                    >
+                                        Conductor
+                                    </NavLink>
+                                )}
+                            </Menu.Item>
+                            <Menu.Item>
+                                {({ active }) => (
+                                    <NavLink
+                                        to="/about-artists"
+                                        className={`${
+                                            active
+                                                ? `text-black `
+                                                : `text-black `
+                                        } group flex w-full items-center rounded-lg px-2 py-2 text-sm`}
+                                    >
+                                        Guest Artists
+                                    </NavLink>
+                                )}
+                            </Menu.Item>
+                            <Menu.Item>
+                                {({ active }) => (
+                                    <NavLink
+                                        to="/about-chorale"
+                                        className={`${
+                                            active
+                                                ? `text-black `
+                                                : `text-black `
+                                        } group flex w-full items-center rounded-lg px-2 py-2 text-sm`}
+                                    >
+                                        Chorale
+                                    </NavLink>
+                                )}
+                            </Menu.Item>
+                            <Menu.Item>
+                                {({ active }) => (
+                                    <NavLink
+                                        to="/about-executive-team"
+                                        className={`${
+                                            active
+                                                ? `text-black `
+                                                : `text-black `
+                                        } group flex w-full items-center rounded-lg px-2 py-2 text-sm`}
+                                    >
+                                        Executive Team
+                                    </NavLink>
+                                )}
+                            </Menu.Item>
+                        </Menu.Items>
+                    </Menu>
+
+                    <NavLink
+                        to="/audition"
+                        className={({ isActive }) =>
+                            isActive ? activeNav : inactiveNav
+                        }
+                    >
+                        Join Us
+                    </NavLink>
+                </div>
+                <div className="flex mr-8">
+                    
                         {user ? (
                             <>
                                 <NavLink
@@ -188,7 +149,7 @@ const NavBar = () => {
                         ) : (
                             <LoginButton className={inactiveNav} />
                         )}
-                    </div>
+                  
                 </div>
             </div>
         </nav>
