7e7c35aff733b81e76323a0d43f71c09e1c02549
Caleb Ratelle <caleb.ratelle@gmail.com>
2023-10-18 17:44:45 -0600

    conductor bio

--- src/Pages/Conductor.jsx
+++ src/Pages/Conductor.jsx
@@ -5,16 +5,15 @@ export default function AboutConductor() {
       <div className="max-w-prose mx-auto flex">
         <img src="Adamhansen.jpg" alt="Adam Hansen" className="w-1/2 p-4" />
         <div className="w-1/2 p-4">
-          {/* Add your content here */}
-          In 2014, Adam was appointed as an organist at St Marks Cathedral in Salt Lake City, Utah. In 2015, he was appointed as the organist for the Sterling Singer chorus. He has studied organ performance at the University of Utah under Dr. Kenneth Udy. He has also studied organ under Dr. Geoffrey Myers and Jane Dye in St. George. 
+          <p>In 2014, Adam was appointed as an organist at St Marks Cathedral in Salt Lake City, Utah. In 2015, he was appointed as the organist for the Sterling Singer chorus. He has studied organ performance at the University of Utah under Dr. Kenneth Udy. He has also studied organ under Dr. Geoffrey Myers and Jane Dye in St. George.</p>
 
-Adam's deep passion for music began at a young age. Adam is an accomplished organist, pianist, vocalist, and conductor. The stage was set during his attendance at Snow Canyon High School where he was the Concert Choir pianist, Madrigal pianist/baritone, and the Assistant Conductor. He also was involved in theater as a cast member in plays and also the orchestra conductor for another play. In 2009, he was named Snow Canyon High School’s Sterling Scholar for Music
+          <p>Adam's deep passion for music began at a young age. Adam is an accomplished organist, pianist, vocalist, and conductor. The stage was set during his attendance at Snow Canyon High School where he was the Concert Choir pianist, Madrigal pianist/baritone, and the Assistant Conductor. He also was involved in theater as a cast member in plays and also the orchestra conductor for another play. In 2009, he was named Snow Canyon High School's Sterling Scholar for Music.</p>
 
-In his youth, he was a regular performer in the historic St. George, Utah Tabernacle. Before moving to Salt Lake City, he started his career in church music at Grace Episcopal Church in St. George. He was appointed as the organist at Grace in 2013. In February of 2014, he had the distinct opportunity to travel to Europe to study under master organists, Margaret Phillips in London and Ullrich Bohme in Leipzig, Germany- in the historic St. Thomas Cathedral. In 2015 he was honored to be selected as a recitalist and the organist in Eucharistic services for the 2015 National Episcopal General Convention in Salt Lake City. As an organist in Salt Lake City, Adam has now performed across northern, Utah including the historic Salt Lake Tabernacle. 
+          <p>In his youth, he was a regular performer in the historic St. George, Utah Tabernacle. Before moving to Salt Lake City, he started his career in church music at Grace Episcopal Church in St. George. He was appointed as the organist at Grace in 2013. In February of 2014, he had the distinct opportunity to travel to Europe to study under master organists, Margaret Phillips in London and Ullrich Bohme in Leipzig, Germany- in the historic St. Thomas Cathedral. In 2015 he was honored to be selected as a recitalist and the organist in Eucharistic services for the 2015 National Episcopal General Convention in Salt Lake City. As an organist in Salt Lake City, Adam has now performed across northern, Utah including the historic Salt Lake Tabernacle.</p>
 
-Adam has experience working in a corporate atmospheres in finance and law. He has developed administrative skills and vision for success. 
+          <p>Adam has experience working in corporate atmospheres in finance and law. He has developed administrative skills and vision for success.</p>
 
-Adams passions include nutrition, physical fitness, aviation, waterskiing, hiking, camping, gardening and reading.
+          <p>Adam's passions include nutrition, physical fitness, aviation, waterskiing, hiking, camping, gardening, and reading.</p>
         </div>
       </div>
     </div>
