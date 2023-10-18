6eab5e2afc7b718459794ee4c3fa79522464c590
Caleb Ratelle <caleb.ratelle@gmail.com>
2023-10-18 10:27:48 -0600

    members navbar link directs to members page

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,45 @@
-import Homepage from './Pages/Homepage';
-import { Navigate, Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
-import axios from 'axios';
-import Layout from './Elements/Layout';
-import ErrorPage from './Pages/Error';
-import Login from './Pages/Login';
-import Conductor from './Pages/Conductor';
-import Affiliate from './Pages/Affiliate';
-import Admin from './Pages/Admin';
-import Events from './Pages/Events';
-import Support from './Pages/Support';
-import Chorale from './Pages/Chorale';
-import Artists from './Pages/Artists';
-import Audition from './Pages/Audition';
-import TermsConditions from './Pages/TermsConditions';
-import Credits from './Pages/Credits';
-import EqualityDiversityPolicy from './Pages/EqualityDiversityPolicy';
-import PrivacyPolicy from './Pages/PrivacyPolicy';
-import Contact from './Pages/Contact';
-import ExecutiveTeam from './Pages/ExecutiveTeam';
+import Homepage from "./Pages/Homepage";
+import {
+  Navigate,
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import axios from "axios";
+import Layout from "./Elements/Layout";
+import ErrorPage from "./Pages/Error";
+import Login from "./Pages/Login";
+import Conductor from "./Pages/Conductor";
+import Affiliate from "./Pages/Affiliate";
+import Admin from "./Pages/Admin";
+import Events from "./Pages/Events";
+import Support from "./Pages/Support";
+import Chorale from "./Pages/Chorale";
+import Artists from "./Pages/Artists";
+import Audition from "./Pages/Audition";
+import TermsConditions from "./Pages/TermsConditions";
+import Credits from "./Pages/Credits";
+import EqualityDiversityPolicy from "./Pages/EqualityDiversityPolicy";
+import PrivacyPolicy from "./Pages/PrivacyPolicy";
+import Contact from "./Pages/Contact";
+import ExecutiveTeam from "./Pages/ExecutiveTeam";
+import Members from "./Pages/Members";
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Layout />} errorElement={<ErrorPage />}>
+        <Route index element={<Homepage />} />
+        <Route path="/terms-conditions" element={<TermsConditions />} />
+        <Route path="/members" element={<Members />} />
+        <Route path="/credits" element={<Credits />} />
         <Route
-          index
-          element={<Homepage />}
+          path="/equality-diversity-policy"
+          element={<EqualityDiversityPolicy />}
         />
-        <Route 
-          path="/terms-conditions" 
-          element={<TermsConditions/>} />
-        <Route 
-          path="/credits" 
-          element={<Credits/>} />
-        <Route 
-          path="/equality-diversity-policy" 
-          element={<EqualityDiversityPolicy/>} />
-        <Route 
-          path="/privacy-policy" 
-          element={<PrivacyPolicy/>} />
-        <Route 
-          path="/contact" 
-          element={<Contact/>} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/contact" element={<Contact />} />
         <Route
           path="/about-conductor"
           element={<Conductor />}
@@ -95,22 +93,19 @@ function App() {
           //   console.log("res.data", res.data);
           //   return { affiliates: res.data };
           // }}
-        /> 
+        />
         <Route
           path="/admin"
-          element={<Admin /> }
+          element={<Admin />}
           // loader={async () => {
           //   const res = await axios.get(`/api/admin`);
           //   return { admin: res.data };
           // }}
         />
-        <Route
-          path="/events"
-          element={<Events/>}
-        />
+        <Route path="/events" element={<Events />} />
         <Route
           path="/support"
-          element={<Support/>}
+          element={<Support />}
           // loader={async () => {
           //   const res = await axios.get(`/api/admin`);
           //   return { admin: res.data };
@@ -119,8 +114,7 @@ function App() {
       </Route>
     )
   );
-  return <RouterProvider router={router}/>
+  return <RouterProvider router={router} />;
 }
 
 export default App;
-
