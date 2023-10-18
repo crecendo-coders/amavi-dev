a983d9b8839417b54b89f9be31dd296b5c6c275b
Caleb Ratelle <caleb.ratelle@gmail.com>
2023-10-18 17:43:57 -0600

    Conductor bio updated

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -6,7 +6,7 @@ export default function Homepage() {
     <div className="bg-gray-100 h-screen flex flex-col justify-center items-center">
       <iframe
         width="80%"
-        height="70%"
+        height="100%"
         src="https://www.youtube.com/embed/NfNPQ6ljU1Q?si=0ZoM14JeOL-Wphx3"
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
