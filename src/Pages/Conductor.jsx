aae46ac99302afa793b75875e59d5271cacc479d
Caleb Ratelle <caleb.ratelle@gmail.com>
2023-10-18 12:55:46 -0600

    about the conductor

--- src/Pages/Conductor.jsx
+++ src/Pages/Conductor.jsx
@@ -1,5 +1,17 @@
 export default function AboutConductor() {
   return (
-    <div>AboutConductor</div>
-  )
+    <div className="text-center p-6">
+      <h2 className="text-3xl font-bold mb-6">About the Conductor</h2>
+      <div className="max-w-prose mx-auto flex">
+        <img src="Adamhansen.jpg" alt="Adam Hansen" className="w-1/2 p-4" />
+        <div className="w-1/2 p-4">
+          {/* Add your content here */}
+          About Adam About Adam About Adam About Adam About Adam About Adam
+          About Adam About Adam About Adam About Adam About Adam About Adam
+          About Adam About Adam About Adam About Adam About Adam About Adam
+          About Adam About Adam About Adam
+        </div>
+      </div>
+    </div>
+  );
 }
