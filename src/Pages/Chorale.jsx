3f0488c0c95c05941a1b06340744c2f91a2366ba
Caleb Ratelle <caleb.ratelle@gmail.com>
2023-10-18 18:25:19 -0600

    styling about choir and conductor

--- src/Pages/Chorale.jsx
+++ src/Pages/Chorale.jsx
@@ -4,10 +4,11 @@ export default function AboutChorale() {
   return (
     <div>
       <div className="text-left p-6">
-        <h2 className="text-4xl font-bold mb-6 text-center">Our Mission</h2>
         <div className="w-3/4 mx-auto flex">
-          <img src="ChoirGroupJPGfile.jpg" alt="Choir" className="w-1/3 p-4" />
-          <div className="w-2/3 p-4 text-lg">
+          <img src="ChoirGroupJPGfile.jpg" alt="Choir" className="w-2/5 p-4" />
+          <div className="w-3/5 p-4 text-lg">
+            <h2 className="text-4xl font-bold mb-6 text-center">Our Mission</h2>
+
             <p>
               Amavi Chorale aspires to contribute to an already rich choral
               tradition along the Utah Wasatch Front by:
@@ -22,9 +23,9 @@ export default function AboutChorale() {
         </div>
       </div>
       <div className="text-left p-6">
-        <h2 className="text-4xl font-bold mb-6 text-center">Our Motivation</h2>
         <div className="w-3/4 mx-auto flex">
-          <div className="w-2/3 p-4 text-lg">
+          <div className="w-3/5 p-4 text-lg">
+        <h2 className="text-4xl font-bold mb-6 text-center">Our Motivation</h2>
             <p>
               Founded in 2017, Amavi Chorale is dedicated to creating meaningful
               musical experiences by giving first rate performances of the
@@ -38,7 +39,11 @@ export default function AboutChorale() {
               God.
             </p>
           </div>
-          <img src="Cathedral(2)JPGfile.jpg" alt="Cathedral Organ" className="w-1/3 p-4" />
+          <img
+            src="Cathedral(2)JPGfile.jpg"
+            alt="Cathedral Organ"
+            className="w-2/5 p-4"
+          />
         </div>
       </div>
     </div>
--- src/Pages/Conductor.jsx
+++ src/Pages/Conductor.jsx
@@ -1,19 +1,55 @@
 export default function AboutConductor() {
   return (
-    <div className="text-left p-6"> 
-      <h2 className="text-4xl font-bold mb-6 text-center">About the Conductor</h2>
+    <div className="text-left p-6">
       <div className="w-3/4 mx-auto flex">
-        <img src="Adamhansen.jpg" alt="Adam Hansen" className="w-1/4 p-4" />
-        <div className="w-3/4 p-4">
-          <p className="text-lg">In 2014, Adam was appointed as an organist at St Marks Cathedral in Salt Lake City, Utah. In 2015, he was appointed as the organist for the Sterling Singer chorus. He has studied organ performance at the University of Utah under Dr. Kenneth Udy. He has also studied organ under Dr. Geoffrey Myers and Jane Dye in St. George.</p>
-          
-          <p className="text-lg">Adam's deep passion for music began at a young age. Adam is an accomplished organist, pianist, vocalist, and conductor. The stage was set during his attendance at Snow Canyon High School where he was the Concert Choir pianist, Madrigal pianist/baritone, and the Assistant Conductor. He also was involved in theater as a cast member in plays and also the orchestra conductor for another play. In 2009, he was named Snow Canyon High School's Sterling Scholar for Music.</p>
-          
-          <p className="text-lg">In his youth, he was a regular performer in the historic St. George, Utah Tabernacle. Before moving to Salt Lake City, he started his career in church music at Grace Episcopal Church in St. George. He was appointed as the organist at Grace in 2013. In February of 2014, he had the distinct opportunity to travel to Europe to study under master organists, Margaret Phillips in London and Ullrich Bohme in Leipzig, Germany- in the historic St. Thomas Cathedral. In 2015 he was honored to be selected as a recitalist and the organist in Eucharistic services for the 2015 National Episcopal General Convention in Salt Lake City. As an organist in Salt Lake City, Adam has now performed across northern, Utah including the historic Salt Lake Tabernacle.</p>
-          
-          <p className="text-lg">Adam has experience working in corporate atmospheres in finance and law. He has developed administrative skills and vision for success.</p>
-          
-          <p className="text-lg">Adam's passions include nutrition, physical fitness, aviation, waterskiing, hiking, camping, gardening, and reading.</p>
+        <img src="Adamhansen.jpg" alt="Adam Hansen" className="w-2/5 p-4" />
+        <div className="w-3/5 p-4">
+        <h2 className="text-4xl font-bold mb-6 text-center">
+          About the Conductor
+        </h2>
+          <p className="text-lg">
+            In 2014, Adam was appointed as an organist at St Marks Cathedral in
+            Salt Lake City, Utah. In 2015, he was appointed as the organist for
+            the Sterling Singer chorus. He has studied organ performance at the
+            University of Utah under Dr. Kenneth Udy. He has also studied organ
+            under Dr. Geoffrey Myers and Jane Dye in St. George.
+          </p>
+
+          <p className="text-lg">
+            Adam's deep passion for music began at a young age. Adam is an
+            accomplished organist, pianist, vocalist, and conductor. The stage
+            was set during his attendance at Snow Canyon High School where he
+            was the Concert Choir pianist, Madrigal pianist/baritone, and the
+            Assistant Conductor. He also was involved in theater as a cast
+            member in plays and also the orchestra conductor for another play.
+            In 2009, he was named Snow Canyon High School's Sterling Scholar for
+            Music.
+          </p>
+
+          <p className="text-lg">
+            In his youth, he was a regular performer in the historic St. George,
+            Utah Tabernacle. Before moving to Salt Lake City, he started his
+            career in church music at Grace Episcopal Church in St. George. He
+            was appointed as the organist at Grace in 2013. In February of 2014,
+            he had the distinct opportunity to travel to Europe to study under
+            master organists, Margaret Phillips in London and Ullrich Bohme in
+            Leipzig, Germany- in the historic St. Thomas Cathedral. In 2015 he
+            was honored to be selected as a recitalist and the organist in
+            Eucharistic services for the 2015 National Episcopal General
+            Convention in Salt Lake City. As an organist in Salt Lake City, Adam
+            has now performed across northern, Utah including the historic Salt
+            Lake Tabernacle.
+          </p>
+
+          <p className="text-lg">
+            Adam has experience working in corporate atmospheres in finance and
+            law. He has developed administrative skills and vision for success.
+          </p>
+
+          <p className="text-lg">
+            Adam's passions include nutrition, physical fitness, aviation,
+            waterskiing, hiking, camping, gardening, and reading.
+          </p>
         </div>
       </div>
     </div>
