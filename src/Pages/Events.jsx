6402fdb4f32b710f11c5f15b02b8da05cb284d29
Caleb Ratelle <caleb.ratelle@gmail.com>
2023-10-19 13:24:34 -0600

    homepage editing event banner(made them each a button too

--- src/Pages/Events.jsx
+++ src/Pages/Events.jsx
@@ -1,38 +1,26 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
 const EventComponent = () => {
-  const [events, setEvents] = useState([]);
-
-  useEffect(() => {
-    // Fetch your events data using axios or your preferred method and set it using setEvents.
-    // Example:
-    axios.get("your_api_endpoint").then((response) => {
-      setEvents(response.data);
-    });
-  }, []);
-
   return (
-    <div>
+    <div className="flex w-4/5 gap-3 my-4">
       <a
-        href="/events/2023/11/14/introit"
-        className="mx-auto bg-gray-100 h-screen flex items-center justify-center px-8"
+        href="/events/2023/11/3/introit"
+        className="bg-gray-100 flex items-center justify-center"
         style={{ textDecoration: "none" }}
       >
-        <div className="flex flex-col w-full bg-white rounded shadow-lg sm:w-3/4 md:w-1/2 lg:w-3/5">
-          <div
-            className="w-full h-64 bg-top bg-cover rounded-t"
-            style={{
-              backgroundImage: `url(https://amaviphotos.s3.us-west-2.amazonaws.com/Event-Banner-Introit.jpg)`,
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-            }}
-          ></div>
+        <div className="flex flex-col w-full bg-white rounded shadow-lg">
+          <div class="w-full h-fit bg-top bg-cover rounded-t flex-col flex justify-end">
+            <img
+              src="https://amaviphotos.s3.us-west-2.amazonaws.com/Event-Banner-Introit.jpg"
+              alt="concert-banner"
+              class="rounded"
+            />
+          </div>
 
           <div className="flex flex-col w-full md:flex-row">
             <div className="flex flex-row justify-around p-4 font-bold leading-none text-gray-800 uppercase bg-gray-400 rounded md:flex-col md:items-center md:justify-center md:w-1/4">
               <div className="md:text-3xl">Nov</div>
-              <div className="md:text-6xl">14</div>
+              <div className="md:text-6xl">3</div>
               <div className="md:text-xl">7:30 pm</div>
             </div>
             <div className="p-4 font-normal text-gray-800 md:w-3/4">
@@ -54,60 +42,24 @@ const EventComponent = () => {
         </div>
       </a>
 
-      <div className="bg-gray-100 p-4">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {events.map((event, i) => (
-            <div key={i} className="bg-white rounded-lg p-4 shadow-md">
-              <div className="flex flex-col w-full bg-white rounded shadow-lg sm:w-3/4 md:w-1/2 lg:w-3/5">
-                <div
-                  className="w-full h-64 bg-top bg-cover rounded-t"
-                  style={{
-                    backgroundImage: `url("https://amaviphotos.s3.us-west-2.amazonaws.com/Introit-Poster.jpg")`,
-                    backgroundSize: "100%",
-                    backgroundRepeat: "no-repeat",
-                  }}
-                ></div>
-                <div className="flex flex-col w-full md:flex-row">
-                  <div className="flex flex-row justify-around p-4 font-bold leading-none text-gray-800 uppercase bg-gray-400 rounded md:flex-col md:items-center md:justify-center md:w-1/4">
-                    <div className="md:text-3xl">{event.date}</div>
-                    <div className="md:text-6xl">{event.date}</div>
-                    <div className="md:text-xl">{event.time}</div>
-                  </div>
-                  <div className="p-4 font-normal text-gray-800 md:w-3/4">
-                    <h3 className="mb-4 text-4xl font-bold leading-none tracking-tight text-gray-800">
-                      {event.name}
-                    </h3>
-                    <p className="leading-normal">{event.summary}</p>
-                    <div className="flex flex-row items-center mt-4 text-gray-700">
-                      <div className="w-1/2">{event.location}</div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-
       <a
-        href="/events/2023/11/15/introit"
-        className="mx-auto bg-gray-100 h-screen flex items-center justify-center px-8"
+        href="/events/2023/11/4/introit"
+        className="bg-gray-100 flex items-center justify-center"
         style={{ textDecoration: "none" }}
       >
-        <div className="flex flex-col w-full bg-white rounded shadow-lg sm:w-3/4 md:w-1/2 lg:w-3/5">
-          <div
-            className="w-full h-64 bg-top bg-cover rounded-t"
-            style={{
-              backgroundImage: `url(https://amaviphotos.s3.us-west-2.amazonaws.com/Event-Banner-Introit.jpg)`,
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-            }}
-          ></div>
+        <div className="flex flex-col w-full bg-white rounded shadow-lg">
+          <div class="w-full h-fit bg-top bg-cover rounded-t flex-col flex justify-end">
+            <img
+              src="https://amaviphotos.s3.us-west-2.amazonaws.com/Event-Banner-Introit.jpg"
+              alt="concert-banner"
+              class="rounded"
+            />
+          </div>
 
           <div className="flex flex-col w-full md:flex-row">
             <div className="flex flex-row justify-around p-4 font-bold leading-none text-gray-800 uppercase bg-gray-400 rounded md:flex-col md:items-center md:justify-center md:w-1/4">
               <div className="md:text-3xl">Nov</div>
-              <div className="md:text-6xl">15</div>
+              <div className="md:text-6xl">4</div>
               <div className="md:text-xl">7:30 pm</div>
             </div>
             <div className="p-4 font-normal text-gray-800 md:w-3/4">
@@ -133,7 +85,6 @@ const EventComponent = () => {
 };
 
 export default EventComponent;
-
 
 // const [events, setEvents] = useState([]);
 
